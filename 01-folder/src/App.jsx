07685edab7da9b04f66e6451755049d6a373555e
Shantanu Mani MIshra <147247776+PewdevCodes@users.noreply.github.com>
2025-12-13 00:59:13 +0530

    Initializing the folder

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => { 
+  return (
+   <> 
+   <div id='dad'>
+    <h1>Hello Words</h1>
+   </div>
+
+   <div id='chacha'>
+    <h2>This is React Folder Structure</h2>
+   </div>
+   </> 
+  )
+}
+
+export default App;
