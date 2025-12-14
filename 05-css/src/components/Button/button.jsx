aa02e1a8034022c12cdcc0cf32664df711cf89a7
Shantanu Mani MIshra <147247776+PewdevCodes@users.noreply.github.com>
2025-12-14 19:42:57 +0530

    Playing with CSS Modules

---
+++ 05-css/src/components/Button/button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './button.module.css'
+const button = () => {
+  return (
+    <div className={styles.btn}>
+      This is Joey
+    </div>
+  )
+}
+
+export default button
---
+++ 05-css/src/components/Header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './header.module.css'
+const header = () => {
+  return (
+    <div className={styles.header}>
+      This is header
+    </div>
+  )
+}
+
+export default header
